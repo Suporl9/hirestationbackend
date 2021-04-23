043d5d8bac82ebae26676745299a522a5a7edee7
suporl <sumanpok9@gmail.com>
2021-04-23 21:26:16 +0545

    added functionality like create update delete service in service controller and tested on postman

--- controller/serviceController.js
+++ controller/serviceController.js
@@ -1,10 +1,10 @@
 const serviceModel = require("../model/serviceModel");
 
-//create new service => localhost/services  GET  and posting in the database
+//create new service => localhost/services/new  POST  and posting in the database
 const postService = async (req, res) => {
   try {
     const service = await serviceModel.create(req.body); //this creates and triggers  the .save to save our req on the database
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       service,
     });
@@ -12,12 +12,85 @@ const postService = async (req, res) => {
     console.log(error);
   }
 };
-
+//get all the services in the database!! GET => services/
 const getAllServices = async (req, res) => {
   try {
-    res.status(200).json({
+    const getServices = await serviceModel.find();
+
+    return res.status(200).json({
       success: true,
-      message: "working  currently but needs to be modified",
+      count: getServices.length,
+      getServices,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//get single sercive title and details!!  GET => services/:id
+const getAService = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const getService = await serviceModel.findById(id);
+    if (!getService) {
+      return res.status(404).json({
+        success: false,
+        message: "Service Not Found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      getService,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const updateService = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateSer = await serviceModel.findById(id); //using let cause  we will be changing the updateSer later on
+
+    if (!updateSer) {
+      return res.status(404).json({
+        success: false,
+        message: "Service not found",
+      });
+    }
+    await updateSer.findByIdAndUpdate(id, req.body, {
+      new: true, //the default is to send the old and unaltered document but if we set new to true  it returns the new altered document!
+      // runValidators: true,
+      useFindAndModify: false,
+    }); //should pass third parameter object to avoid the deprication warning like findoneandupdate without useFindandModify  set to false are depricated
+
+    return res.status(201).json({
+      success: true,
+      updateSer,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+//now for the deleting the service by id provided from the user
+
+const deleteService = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteServ = await serviceModel.findById(id);
+
+    if (!deleteServ) {
+      return res.status(404).json({
+        success: false,
+        message: "Service not found",
+      });
+    }
+    await deleteServ.remove();
+
+    return res.status(200).json({
+      success: true,
+      message: "successfully deleted the service",
+      data: {},
     });
   } catch (error) {
     console.log(error);
@@ -27,4 +100,7 @@ const getAllServices = async (req, res) => {
 module.exports = {
   getAllServices,
   postService,
+  getAService,
+  updateService,
+  deleteService,
 };
--- routes/serviceRouter.js
+++ routes/serviceRouter.js
@@ -5,8 +5,19 @@ const auth = require("../middleware/auth");
 
 //auth is a custom middleware to verifying the token and creating customer if only there is the token or user is logged in already
 
-router.route("/").post(auth, serviceController.postService); //if the user is logged in or has the token still available then only he will be able to create a new customer or else it would be unauthorized
+router.route("/new").post(auth, serviceController.postService); //if the user is logged in or has the token still available then only he will be able to create a new customer or else it would be unauthorized
 
+//get all the services in the database
 router.route("/").get(auth, serviceController.getAllServices);
+
+//get  a single service according to the id passed from the database
+router.route("/:id").get(auth, serviceController.getAService);
+
+//update the service //for eg the title and other body can be updated
+
+router.route("/:id").put(auth, serviceController.updateService);
+
+//for deleting  the service //according the objectid  ofcourse
+router.route("/:id").delete(auth, serviceController.deleteService);
 
 module.exports = router;
